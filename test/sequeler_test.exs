03fb0cf4a4e30b84f0bb964c0f9fa5d75d92cf86
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-08-30 16:29:57 +0200

    Get db into testing

--- test/sequeler_test.exs
+++ test/sequeler_test.exs
@@ -1,8 +1,26 @@
+require Logger, as: L
+
 defmodule SequelerTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+
+    # add emysql pool
+    L.info "Creating the pool..."
+    :emysql.add_pool(:db, [ size: 50,
+      user: 'testuser', password: 'testpassword',
+      database: 'testdb', encoding: :utf8 ])
+
+    on_exit fn ->
+      L.info "Removing the pool..."
+      :emysql.remove_pool :db
+    end
+
+    :ok
+  end
+
+  test "the pool" do
+    :emysql.execute :db, "select sleep(2)"
   end
 end
