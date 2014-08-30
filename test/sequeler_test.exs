decb1432ae6c3af54e04fbbb41cf0f7ffcd0f2aa
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-08-30 16:32:58 +0200

    No need to get db into testing, it was already there...

--- test/sequeler_test.exs
+++ test/sequeler_test.exs
@@ -4,22 +4,6 @@ defmodule SequelerTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  setup_all do
-
-    # add emysql pool
-    L.info "Creating the pool..."
-    :emysql.add_pool(:db, [ size: 50,
-      user: 'testuser', password: 'testpassword',
-      database: 'testdb', encoding: :utf8 ])
-
-    on_exit fn ->
-      L.info "Removing the pool..."
-      :emysql.remove_pool :db
-    end
-
-    :ok
-  end
-
   test "the pool" do
     :emysql.execute :db, "select sleep(2)"
   end
