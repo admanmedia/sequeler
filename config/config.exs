10bbd01636941a2b52b26056241915dd99ce2c17
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-12-06 08:38:40 +0100

    Evolve config

--- config/config.exs
+++ config/config.exs
@@ -25,5 +25,10 @@ config :logger, :console,
 config :sequeler, :key_phrase, "testingphrasesforencryption"
 config :sequeler, :iv_phrase, "testingphrasesforencryption"
 
+config :bottler, :servers, [server1: [public_ip: "123.123.123.123"],
+                             server2:  [public_ip: "123.123.123.123"]]
+
 config :sequeler, :db_opts, [ size: 50, user: 'testuser',
       password: 'testpassword', database: 'testdb', encoding: :utf8 ]
+
+import_config "#{Mix.env}.exs"
