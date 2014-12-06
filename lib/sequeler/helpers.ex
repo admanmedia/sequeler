aacf723edd3c7ae6f1f7af01830a448705997d75
Rubén Caro <ruben.caro.estevez@gmail.com>
2014-12-06 08:45:00 +0100

    Improve helpers

--- lib/sequeler/helpers.ex
+++ lib/sequeler/helpers.ex
@@ -11,6 +11,10 @@ defmodule Sequeler.Helpers do
     end
   end
 
+  @doc """
+    Execute the query against the underlying `emysql`.
+    Return something serializable to JSON.
+  """
   def query(db,sql) do
     res = :emysql.execute(db,sql)
     case res do
@@ -22,9 +26,30 @@ defmodule Sequeler.Helpers do
     end
   end
 
-  def spit(obj, caller) do
-    %{file: file, line: line} = caller
-    "\n\n#{file}:#{line}\n\n#{inspect obj}\n\n" |> L.debug
+  def spit(obj, caller \\ nil) do
+    loc = case caller do
+      %{file: file, line: line} -> "\n\n#{file}:#{line}"
+      _ -> ""
+    end
+    "#{loc}\n\n#{inspect obj}\n\n" |> L.debug
+  end
+
+  defmodule FileWatcher do
+
+    @doc """
+      Perform harakiri if given file is touched. Else keep an infinite loop
+      sleeping given msecs each time.
+    """
+    def loop(path, previous_mtime \\ nil, sleep_ms \\ 5_000) do
+      new_mtime = File.stat! path
+      if previous_mtime and previous_mtime != new_mtime do
+        :init.stop
+      else
+        :timer.sleep sleep_ms
+        loop path, new_mtime
+      end
+    end
+
   end
 
 end
