b0785ff206234ad762be891234234291717c4c59
Rubén Caro <ruben@elpulgardelpanda.com>
2015-01-02 14:14:56 +0100

    Fix prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -4,10 +4,11 @@ config :sequeler, :key_phrase, "unomasuno"
 config :sequeler, :iv_phrase, "memomimamamemima"
 
 servers = [
-  conrad: [public_ip: '176.58.117.234', private_ip: '192.168.165.72'],
-  bardeen:[public_ip: '176.58.102.177', private_ip: '192.168.164.110']
+  conrad:  [public_ip: '176.58.117.234', private_ip: '192.168.165.72'],
+  bardeen: [public_ip: '176.58.102.177', private_ip: '192.168.164.110']
 ]
-config :bottler, :servers, servers
+
+config :bottler, :servers, [conrad: servers[:conrad]]
 
 config :sequeler, :db_opts, [ size: 5, user: 'epdp',
       password: 'elpulgardb', database: 'rita_production', encoding: :utf8,
